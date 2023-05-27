da99876ff53dcf97ac28a36f6bfafea21e4655d4
ChinaiArman <chinaiarman@gmail.com>
2023-05-27 02:36:23 -0700

    Create a class for constructing the pdf

--- pdfConstructor.py
+++ pdfConstructor.py
@@ -1,6 +1,26 @@
 # Import packages
 from fpdf import FPDF
 import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import argparse
 
 # Import local files
 import analyticsWizard as aw
+import pdfWizard as pw
+
+# Constants
+FILENAME = "" # UPDATE
+
+
+class pdfConstructor:
+    def __init__(self, new_df, new_root):
+        self.df = new_df
+        self.root = new_root
+        self.path = new_root + FILENAME # UPDATE
+        
+
+if __name__ == "__main__":
+    pass
---
+++ pdfWizard.py
@@ -0,0 +1,10 @@
+# Import packages
+import argparse
+import pandas as pd
+
+# Import local files
+import pdfConstructor
+
+
+if __name__ == "__main__":
+    pass
