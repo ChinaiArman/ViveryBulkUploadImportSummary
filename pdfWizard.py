c831164064ff031ac2d13fdf70f01624b99db4f7
ChinaiArman <chinaiarman@gmail.com>
2023-05-27 13:47:23 -0700

    Merge constructor and wizard into one file

--- pdfWizard.py
+++ pdfWizard.py
@@ -1,9 +1,30 @@
 # Import packages
-import argparse
+from fpdf import FPDF
+import os
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import argparse
 
 # Import local files
-from pdfConstructor import pdfConstructor
+import analyticsWizard as aw
+
+# Constants
+FILENAME = "" # UPDATE
+
+
+class pdfConstructor:
+    def __init__(self, 
+                 new_df: pd.DataFrame(), 
+                 new_root: str
+                ):
+        self.df = new_df
+        self.root = new_root
+        self.path = new_root + FILENAME # UPDATE
+        
+    def print(self):
+        print(self.df)
 
 
 if __name__ == "__main__":
