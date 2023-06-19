07f5e1cb87effbfb021cff3e66d1e5edfded777c
ChinaiArman <chinaiarman@gmail.com>
2023-06-19 12:29:27 -0700

    Create text.py

---
+++ text.py
@@ -0,0 +1,152 @@
+TITLE_PAGE = {
+    "title": "",
+}
+
+TABLE_OF_CONTENTS = {
+    "title": "",
+}
+
+LOCATION_MAP = {
+    "title": "",
+}
+
+NETWORK_OVERVIEW = {
+    "title": "",
+}
+
+PROFILE_COMPLETENESS = {
+    "title": "",
+}
+
+HIGH_LOW_PROFILE_GRADES = {
+    "title": "",
+}
+
+VIVERY_CONTACT_INFORMATION = {
+    "title": "",
+}
+
+PUBLIC_CONTACT_INFORMATION = {
+    "title": "",
+}
+
+PROGRAM_TYPES = {
+    "title": "",
+}
+
+PROGRAM_FILTER_FIELDS = {
+    "title": "",
+}
+
+RECOMMENDED_PROGRAM_FILTER_OPTIONS = {
+    "title": "",
+}
+
+NETWORK_HOURS_OVERVIEW = {
+    "title": "",
+}
+
+NETWORK_HOUR_TYPE_USAGE = {
+    "title": "",
+}
+
+LOCATION_HOURS_PREVIEW = {
+    "title": "",
+}
+
+PROGRAM_HOURS_PREVIEW = {
+    "title": "",
+}
+
+MISSING_PROGRAM_QUALIFICATIONS = {
+    "title": "",
+}
+
+MISSING_PROGRAM_SERVICE_AREA = {
+    "title": "",
+}
+
+EXAMPLE_PROFILE_PREVIEW = {
+    "title": "",
+}
+
+APPENDIX_ORGANIZATION_LIST = {
+    "title": "",
+}
+
+APPENDIX_LOCATION_LIST = {
+    "title": "",
+}
+
+
+APPENDIX_PROGRAM_LIST = {
+    "title": "",
+}
+
+APPENDIX_PROGRAM_PROFILE_COMPLETION_TIERS = {
+    "title": "",
+}
+
+APPENDIX_PROGRAM_CATEGORY_FIELD_WEIGHTS = {
+    "title": "",
+}
+
+APPENDIX_PROGRAM_PROFILE_COMPLETION_LIST = {
+    "title": "",
+}
+
+APPENDIX_ORGANIZATION_CONTACT_INFORMATION = {
+    "title": "",
+}
+
+APPENDIX_LOCATION_CONTACT_INFORMATION = {
+    "title": "",
+}
+
+APPENDIX_PROGRAM_CONTACT_INFORMATION = {
+    "title": "",
+}
+
+APPENDIX_PROGRAM_TYPE = {
+    "title": "",
+}
+
+APPENDIX_PROGRAM_AUDIENCE = {
+    "title": "",
+}
+
+APPENDIX_PROGRAM_LANGUAGES_SPOKEN = {
+    "title": "",
+}
+
+APPENDIX_PROGRAM_FEATURES = {
+    "title": "",
+}
+
+APPENDIX_PROGRAM_ITEMS_OFFERED = {
+    "title": "",
+}
+
+APPENDIX_PROGRAM_DIETARY_OPTIONS = {
+    "title": "",
+}
+
+APPENDIX_PROGRAM_FILTERS_AVAILABLE = {
+    "title": "",
+}
+
+APPENDIX_LOCATION_HOURS_INFORMATION = {
+    "title": "",
+}
+
+APPENDIX_PROGRAM_HOURS_INFORMATION = {
+    "title": "",
+}
+
+APPENDIX_PROGRAM_QUALIFICATIONS = {
+    "title": "",
+}
+
+APPENDIX_PROGRAM_SERVICE_AREAS = {
+    "title": "",
+}
