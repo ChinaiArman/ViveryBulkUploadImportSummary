19d8973072ef7a3e09626e2e10359dd11477161a
ChinaiArman <chinaiarman@gmail.com>
2023-07-07 14:01:40 -0700

    Fix main file functionality, add more class params

--- pdfWizard.py
+++ pdfWizard.py
@@ -24,18 +24,21 @@ with open(TEXT_SAVE_NAME) as file: TEXT = json.load(file)                       
 class pdfConstructor:
     """
     """
-    def __init__(self, 
-                 new_df: pd.DataFrame(), 
-                 new_directory: str,
-                 new_filepath: str,
-                 new_filename: str
-                ):
+    def __init__(
+            self, 
+            new_df: pd.DataFrame(), 
+            new_directory: str,
+            new_filename: str,
+            new_network_name: str
+            ) -> None:
         """
         """
         self.df = new_df
         self.directory = new_directory
-        self.filepath = new_filepath
         self.filename = new_filename
+        self.network_name = new_network_name
+        self.pdf = FPDF()
+        return
 
 
 
@@ -46,26 +49,33 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Create data visualizations for a Pre-Validated file")
     # Add file argument
     parser.add_argument("file", action="store", help="The file to validate.")
+    # Add network name argument
+    parser.add_argument("network_name", action="store", help="To name the PDF and customize to the specific Network")
     # Console arguments
     args = parser.parse_args()
     
-    # Create filename
-    filename = args.file.split("\\")[-1]
     # Create directory name
-    directory = "data_" + filename.replace(".csv", "")
-    # Create path to file
-    path = directory + "\\" + filename
+    directory = "data_" + args.file.split("\\")[-1].replace(".csv", "")
+    # Create network name
+    network_name = args.network_name
     # Create DataFrame
     df = pd.read_csv(args.file)
 
     # Create directory within project folder
     if not os.path.isdir(directory):
         os.mkdir(directory)
+    if not os.path.isdir(directory + "/resources"):
+        os.mkdir(directory + "/resources")
+    if not os.path.isdir(directory + "/csvs"):
+        os.mkdir(directory + "/csvs")
+    if not os.path.isdir(directory + "/images"):
+        os.mkdir(directory + "/images")
     # Move file to directory
     if args.file.split("\\")[0] != directory:
         shutil.move(args.file, directory)
 
     # Create pdfConstructor instance
-    constructor = pdfConstructor(df, directory, path, filename)
+    constructor = pdfConstructor(df, directory, network_name.replace(" ", "_").lower() + "_analytical_report.pdf", network_name)
 
     # Create PDF
+    constructor.show_data()
