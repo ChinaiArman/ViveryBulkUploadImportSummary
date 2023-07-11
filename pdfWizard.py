6044d0455072255e3a6be1d8e527b6f3824344c7
ChinaiArman <chinaiarman@gmail.com>
2023-07-11 15:14:30 -0700

    Dynamically calculate width and height based on a height value

--- pdfWizard.py
+++ pdfWizard.py
@@ -8,6 +8,7 @@ import pandas as pd                 # Pandas, used to represent CSVs and large d
 import argparse, os, shutil         # Argparse, OS, and Shutil, used for File Manipulation and the Command Line Interface
 import json                         # JSON, used to parse JSON files and convert to Dictionary data types.
 from datetime import date           #
+from PIL import Image
 import calendar
 import glob
 import os
@@ -26,8 +27,8 @@ PROFILE_COMPLETION_TIERS_SAVE_NAME = 'resources/profile_completion_tiers.csv'   
 PROFILE_COMPLETION_TIERS = pd.read_csv(PROFILE_COMPLETION_TIERS_SAVE_NAME)                                      # PROFILE_COMPLETION_TIERS, used to store the profile completion tiers for locations, stored in the file, 'resources/profile_completion_tiers.csv'
 
 # MISC CONSTANTS
-WIDTH = 8.5                         #
-HEIGHT = 11                         #
+PAGE_WIDTH = 8.5                         #
+PAGE_HEIGHT = 11                         #
 
 # COLOURS
 
@@ -80,7 +81,7 @@ class pdfConstructor:
         self.pdf.set_auto_page_break(0)
         self.pdf.set_left_margin(0)
         self.pdf.set_right_margin(0)
-        self.pdf.image("resources\images\cover.png", 0, 0, WIDTH, HEIGHT)
+        self.pdf.image("resources\images\cover.png", 0, 0, PAGE_WIDTH, PAGE_HEIGHT)
 
         # Set Date Font
         self.pdf.set_xy(0.6, 8.5)
@@ -114,7 +115,7 @@ class pdfConstructor:
         self.pdf.add_page()
         if self.pdf.page_no() > 1:
             self.pdf.set_right_margin(0.5)
-            self.pdf.set_y(HEIGHT - 0.5)
+            self.pdf.set_y(PAGE_HEIGHT - 0.5)
             self.pdf.set_text_color(0, 72, 61)
             self.pdf.set_font('Roobert Light', '', 10)
             self.pdf.cell(0, 0, '%s' % self.pdf.page_no(), align='R')
@@ -135,22 +136,28 @@ class pdfConstructor:
         return
     
 
-    def add_image(self, filepath: str, w: int, h: int, pagenumber: int=-2) -> None:
+    def add_image(self, filepath: str, height: int, pagenumber: int=-2) -> None:
         """
         """
+        real_width, real_height = Image.open(filepath).size
+        width = (real_width * height)/real_height
+
         if pagenumber > -2:
             pagelink = self.pdf.add_link()
             self.pdf.set_link(pagelink, page=pagenumber)
         else:
             pagelink = None
-        self.pdf.image(filepath, (WIDTH - w)/2, FPDF.get_y(self.pdf), w, h, link=pagelink)
-        self.pdf.ln(h)
+        self.pdf.image(filepath, (PAGE_WIDTH - width)/2, FPDF.get_y(self.pdf), h=height, link=pagelink)
+        self.pdf.ln(height)
         return
     
 
-    def add_two_images(self, filepath_one: str, filepath_two: str, w: int, h: int, pagenumber_one: int=-2, pagenumber_two: int=-2) -> None:
+    def add_two_images(self, filepath_one: str, filepath_two: str, height: int, pagenumber_one: int=-2, pagenumber_two: int=-2) -> None:
         """
         """
+        real_width, real_height = Image.open(filepath_one).size
+        width = (real_width * height)/real_height
+
         if pagenumber_one > -2:
             pagelink_one = self.pdf.add_link()
             self.pdf.set_link(pagelink_one, page=pagenumber_one)
@@ -162,9 +169,9 @@ class pdfConstructor:
         else:
             pagelink_two = None
         current_y = FPDF.get_y(self.pdf)
-        self.pdf.image(filepath_one, (WIDTH - w*2)/2, current_y, w, h, link=pagelink_one)
-        self.pdf.image(filepath_two, ((WIDTH - w*2)/2) + w, current_y, w, h, link=pagelink_one)
-        self.pdf.ln(h)
+        self.pdf.image(filepath_one, (PAGE_WIDTH - width*2)/2, current_y, h=height, link=pagelink_one)
+        self.pdf.image(filepath_two, ((PAGE_WIDTH - width*2)/2) + width, current_y, h=height, link=pagelink_one)
+        self.pdf.ln(height)
         return
 
 
@@ -208,7 +215,7 @@ class pdfConstructor:
         self.pdf.ln(0.05)
         self.pdf.set_draw_color(0, 72, 61)
         self.pdf.set_line_width(0.05)
-        self.pdf.line(1, FPDF.get_y(self.pdf), WIDTH - 1, FPDF.get_y(self.pdf))
+        self.pdf.line(1, FPDF.get_y(self.pdf), PAGE_WIDTH - 1, FPDF.get_y(self.pdf))
         return
 
 
@@ -235,7 +242,7 @@ class pdfConstructor:
         self.pdf.set_text_color(250, 249, 246)
         self.pdf.set_font('Roobert Medium', '', 14)
         for element in list(df_copy.columns):
-            self.pdf.cell((WIDTH-2)/columns, self.pdf.font_size + 0.2, element, align='C', fill=True, link=pagelink)
+            self.pdf.cell((PAGE_WIDTH-2)/columns, self.pdf.font_size + 0.2, element, align='C', fill=True, link=pagelink)
         self.pdf.ln(self.pdf.font_size + 0.2)
         self.pdf.set_x(1)
 
@@ -244,7 +251,7 @@ class pdfConstructor:
         self.pdf.set_font('Roobert Regular', '', 11)
         for row in list_of_lists:
             for datum in row:
-                self.pdf.cell((WIDTH-2)/columns, self.pdf.font_size + 0.2, str(datum), align='C', link=pagelink)
+                self.pdf.cell((PAGE_WIDTH-2)/columns, self.pdf.font_size + 0.2, str(datum), align='C', link=pagelink)
             self.pdf.ln(self.pdf.font_size + 0.2)
             self.pdf.set_x(1)
         
@@ -253,7 +260,7 @@ class pdfConstructor:
         return
 
 
-    def add_table_header(self, header_row: list, pagenumber: int=-2, padding=True) -> None:
+    def add_h2_text(self, header_row: list, pagenumber: int=-2, padding=True) -> None:
         """
         """
         # Define number of columns
@@ -272,10 +279,10 @@ class pdfConstructor:
         self.pdf.set_text_color(250, 249, 246)
         self.pdf.set_font('Roobert Medium', '', 14)
         if len(header_row) == 1:
-            self.pdf.multi_cell(WIDTH-2, self.pdf.font_size + 0.2, header_row[0], align='C', fill=True, link=pagelink)
+            self.pdf.multi_cell(PAGE_WIDTH-2, self.pdf.font_size + 0.2, header_row[0], align='C', fill=True, link=pagelink)
         else:
             for element in header_row:
-                self.pdf.cell((WIDTH-2)/columns, self.pdf.font_size + 0.2, element, align='C', fill=True, link=pagelink)
+                self.pdf.cell((PAGE_WIDTH-2)/columns, self.pdf.font_size + 0.2, element, align='C', fill=True, link=pagelink)
         if padding:
             self.pdf.ln(self.pdf.font_size + 0.2)
         self.pdf.set_x(1)
@@ -340,7 +347,7 @@ if __name__ == "__main__":
     # Location Map
     constructor.add_page()
     constructor.add_h1_text(TEXT["LOCATION MAP"]["title"])
-    constructor.add_image(ae.create_map(df, directory), 6.5, 4.2)
+    constructor.add_image(ae.create_map(df, directory), 4.2)
     constructor.add_subtitle_text(TEXT["LOCATION MAP"]["subtitle"])
     constructor.add_normal_text(TEXT["LOCATION MAP"]["paragraph"], alignment='C')
 
@@ -356,12 +363,12 @@ if __name__ == "__main__":
     constructor.add_h1_text(TEXT["PROFILE COMPLETENESS"]["title"])
     constructor.add_horizontal_line()
     constructor.add_normal_text(TEXT["PROFILE COMPLETENESS"]["paragraph"])
-    constructor.add_image(ae.graph_profile_grade(df, directory), 5, 3.75)
+    constructor.add_image(ae.graph_profile_grade(df, directory), 3.75)
     constructor.add_subtitle_text(TEXT["PROFILE COMPLETENESS"]["subtitle"])
 
     # Highest Lowest Profile Grades
     constructor.add_h1_text(TEXT["HIGH LOW PROFILE GRADES"]["title"])
-    constructor.add_table_header(TEXT["HIGH LOW PROFILE GRADES"]["header row"])
+    constructor.add_h2_text(TEXT["HIGH LOW PROFILE GRADES"]["header row"])
     constructor.add_table(ae.create_high_low_graded_profiles_table)
     constructor.add_horizontal_line()
     constructor.add_normal_text(TEXT["HIGH LOW PROFILE GRADES"]["paragraph"])
@@ -371,26 +378,32 @@ if __name__ == "__main__":
     constructor.add_h1_text(TEXT["VIVERY CONTACT INFORMATION"]["title"])
     constructor.add_horizontal_line()
     constructor.add_normal_text(TEXT["VIVERY CONTACT INFORMATION"]["paragraph"])
-    constructor.add_image(ae.graph_missing_organization_contact_info(df, directory), 5, 3.75)
+    constructor.add_image(ae.graph_missing_organization_contact_info(df, directory), 3.75)
 
     # Public Contact Information
     constructor.add_h1_text(TEXT["PUBLIC CONTACT INFORMATION"]["title"])
     constructor.add_horizontal_line()
     TEXT = ae.calculate_locations_programs_without_contact(df, TEXT, "PUBLIC CONTACT INFORMATION", "paragraph")
     constructor.add_normal_text(TEXT["PUBLIC CONTACT INFORMATION"]["paragraph"])
-    constructor.add_vertical_space(0.05)
-    constructor.add_table_header(TEXT["PUBLIC CONTACT INFORMATION"]["subtitle"], padding=False)
+    constructor.add_vertical_space(0.075)
+    constructor.add_h2_text(TEXT["PUBLIC CONTACT INFORMATION"]["subtitle"], padding=False)
     constructor.add_vertical_space(0.01)
-    constructor.add_two_images(ae.graph_missing_location_contact_info(df, directory), ae.graph_missing_program_contact_info(df, directory), 3, 2.25)
+    constructor.add_two_images(ae.graph_missing_location_contact_info(df, directory), ae.graph_missing_program_contact_info(df, directory), 2.25)
 
     # Program Types
     constructor.add_page()
     constructor.add_h1_text(TEXT["PROGRAM TYPES"]["title"])
     constructor.add_horizontal_line()
     constructor.add_normal_text(TEXT["PROGRAM TYPES"]["paragraph one"])
-    constructor.add_image(ae.graph_program_type(df, directory), 3.25, 3.25)
+    constructor.add_image(ae.graph_program_type(df, directory), 3.25)
     constructor.add_horizontal_line()
-    
+    constructor.add_vertical_space(0.1)
+    TEXT = ae.calculate_food_distribution_program_percent(df, TEXT, "PROGRAM TYPES", "paragraph two")
+    constructor.add_normal_text(TEXT["PROGRAM TYPES"]["paragraph two"])
+    constructor.add_vertical_space(0.075)
+    constructor.add_h2_text(TEXT["PROGRAM TYPES"]["subtitle"], padding=False)
+    constructor.add_vertical_space(0.01)
+    constructor.add_image(ae.graph_food_program_breakdown(df, directory), 3.25)
 
     # Save PDF
     constructor.save_pdf()
